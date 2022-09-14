f198d21d4eeefb9f42a090ac4cd02415830c1e62
aozturk94 <aslihan37gungoren@gmail.com>
2022-09-14 14:15:44 +0300

    MyFirstApp

---
+++ MyFirstApp/Program.cs
@@ -0,0 +1,45 @@
+﻿//namespace MyFirstApp
+//{
+
+//}//{
+// //    internal class Program                                         
+// //    {
+// //        //iki adet "/" işareti yanyana konarak kod bloklarımıza açıklama yapabiliriz.
+// //        /*Açıklama yazıları tek satırdan oluşmuyorsa
+// //         yıldız taksim satırları arasına alt alta satırlar eklenerek açıklama satırları blokları oluşur.
+// //         */
+// //        //Comment blockları oluşturmak için kullanacağımız kısayol (shortcut) CTRL+K+C kombinasyonudur.
+// //        //Kısayol denemesi
+// //        //Lorem ipsum dolor sit amet
+// //        //Wissen Akademi ERP sınıfı
+// //        //Birinci Hafta VisualStudio Eğitimi 
+// //        static void Main(string[] args)
+//        {
+//            //ilk konsol metodumuz Console Writeline.Konsola yazdırılan komutu bir satır boyunca  tutar.
+
+//            Console.WriteLine("Hello, World!");
+//            //Console WriteLine'a göre farklı bir satır boyunca tutulmasıdır.Kendinden sonra gelen kod bloğu hemen yanından devam eder.
+//            Console.Write("I ASLI");
+
+//            //Kullanıcıdan gelen verieri okumak için kullanılan konsol metodur.
+//            Console.ReadLine();
+
+//            //Kod bitimine eklenerek konsolun kod tamamlanınca kapanmasını engeller.
+//            Console.ReadKey();
+//        }
+//    }
+//}
+//   int a = 5;
+//   int b = 10;
+//string name = "aslıhan ";
+//Console.WriteLine("isminiz " +name+" "+a+" "+b);
+
+////ÖRNEK:
+
+//Console.WriteLine("İsminizi Giriniz.");
+//string isim=Console.ReadLine();
+//Console.WriteLine("Soyadınızı Giriniz.");
+//string soyad=Console.ReadLine();
+//Console.WriteLine("adresinizi giriniz.");
+//string adres=Console.ReadLine();
+//Console.WriteLine("isminiz: " + isim + "  soyadınız : " + soyad + " adresiniz: " + adres );
