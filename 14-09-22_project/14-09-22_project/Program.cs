6c7f54921503aff5618817a381de8ce0854d646f
aozturk94 <aslihan37gungoren@gmail.com>
2022-09-14 14:08:45 +0300

    14-09-22_project

---
+++ 14-09-22_project/14-09-22_project/Program.cs
@@ -0,0 +1,114 @@
+﻿//namespace _14_09_22_project
+//{
+//    internal class Program
+////    {
+////        static void Main(string[] args)
+////        {
+////            Console.WriteLine("---------------------VERİTİPLERİ-----------");
+////            Console.WriteLine("1) Byte:  "); 
+////            Console.WriteLine("Minimum Değer: "+ byte.MinValue);
+////            Console.WriteLine("Maksimum Değer: "+byte.MaxValue);
+////            Console.ReadKey();
+////        }
+////    }
+////}
+
+//byte minDeger = byte.MinValue;
+//byte maxDeger = byte.MaxValue;
+//byte bellekbyte=sizeof(byte);
+//      Console.WriteLine("Minimum Değer "+ minDeger +" Maksimum Değer" + maxDeger);
+//Console.WriteLine($"minimum değer:{minDeger} Maksimum değer:{maxDeger} Byte değeri:{bellekbyte}");
+
+//Örnek:u shortla maks min değer
+//ushort minDeger=ushort.MinValue;  
+//ushort maxDeger=ushort.MaxValue;
+//ushort bellekUShort=sizeof(ushort);
+//Console.WriteLine($"minimum değer: {minDeger} maksimum değer:{maxDeger}");
+
+//Örnek:uint maks min boyut hesapla
+//uint minDeger=uint.MinValue;
+//uint maxDeger=uint.MaxValue;
+//uint bellekUint=sizeof(uint);
+//Console.WriteLine($"minimum değer:{minDeger} Maksimum değer:{maxDeger} uint değeri:{bellekUint}");
+
+//Örnek:Ulong
+// ulong birimDeger = 8;
+// ulong bellekUlong=sizeof(ulong);
+//Console.WriteLine($"değeri:{bellekUlong*birimDeger}");
+
+//Console.WriteLine("b)İşaretli tam sayılar (signed)");
+//Console.WriteLine();
+
+//sbyte birimDeger = 8;
+//sbyte bellekSbyte=sizeof(sbyte);
+//Console.WriteLine($"bellekte kapladığı alan:{birimDeger * bellekSbyte}"); 
+//Console.WriteLine($"***********");
+
+//short birimDeger_short=8;
+//short bellekDeger_short = sizeof(short);
+//Console.WriteLine($"bellekte kapladığı alan:{birimDeger_short * bellekDeger_short}");
+//Console.WriteLine($"***********");
+
+//int birimDeger_int=8;
+//int bellekDeger_int=sizeof(int);
+//Console.WriteLine($"bellekte kapladığı alan:{birimDeger_int* bellekDeger_int}");
+//Console.WriteLine($"**********");
+
+//long birimDeger_long=8;
+//long bellekDeger_long=sizeof(long);
+//Console.WriteLine($"bellekte kapladığı alan:{birimDeger_long* bellekDeger_long}");
+//Console.WriteLine("***********");
+
+//örnek:float örneği
+
+//float minDegerFloat = float.MinValue;
+//float maxDegerFloat=float.MaxValue;
+//float bellekFloat=sizeof(float);
+//Console.WriteLine($"Float tipinde minimum değeri {minDegerFloat} maksimum değeri{maxDegerFloat} bellekte kapladığı alan{bellekFloat} 'dır.");
+
+////örnek:double örneği
+//double minDegerdouble=double.MinValue;
+//double maxDegerdouble=double.MaxValue;
+//double bellekdouble=sizeof(double);
+//ConsoleWriteLine($"Double tipinde minimum değeri {minDegerdouble} maksimum değeri{maxDegerdouble} bellekte kapla.dığı alan{bellekdouble}'dır.");
+
+////örnek:decimal örneği
+
+//decimal minDegerdecimal=decimal.MinValue;
+//decimal maxDegerdecimal=decimal.MaxValue;
+//decimal bellekdecimal=decimal.MaxValue;
+//Console.WriteLine($"Decimal tipinde minimum değeri{minDegerdecimal} maksimum değeri{maxDegerdecimal} bellekte kapladığı alan {bellekdecimal}'dır");
+
+//örnekler:
+//decimal sayi123 = 10;
+//decimal sayi124 = 10.8m;
+//float sayi1 = 20.5f;
+//double sayi2 = 20.5;
+//double sayi3 = 20;
+//Console.WriteLine(sayi123);
+//Console.WriteLine(sayi124);
+
+decimal sayi123 = 10;
+decimal sayi124 = 10.8m;
+float sayi1 = 20.5f;
+double sayi2 = 20.5;
+double sayi3 = 20;
+
+Console.WriteLine("Char tipi");
+char minDegerChar=char.MinValue;
+char maxDegerChar=char.MaxValue;
+Console.WriteLine((int)minDegerChar);
+Console.WriteLine((int)maxDegerChar);
+
+//char karakter tek tırnak ile yazılır.
+char harf1 = 'C';
+Console.WriteLine(harf1);
+
+Console.WriteLine("Bool tipi değişkenler (Mantıksal veri tipi");
+
+
+
+
+
+
+
