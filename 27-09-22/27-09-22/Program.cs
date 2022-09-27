c61d65b6d5d1b514533bcbe35b6844ed1fc91082
aozturk94 <aslihan37gungoren@gmail.com>
2022-09-27 13:42:37 +0300

    27-09-22
    İLK DERS

---
+++ 27-09-22/27-09-22/Program.cs
@@ -0,0 +1,173 @@
+﻿using System;
+
+namespace _27_09_22
+{
+    internal class Program
+    {
+
+
+        //kenar uzunlukları girilen ve üçgen çeşidini yazan program 
+        //public static string Ücgencesidi(int a, int b, int c)
+        //{
+        //    if (a == b && c == b)
+        //    {
+
+        //        return "Eskenar";
+
+        //    }
+        //    else if (b == c || a == b || a == c)
+        //    {
+                
+        //        return "İkizkenar" ;
+        //    }
+        //    else 
+        //    {
+                
+        //        return "çeşitkenar";
+        //    }
+        //}
+
+
+
+
+
+
+
+
+
+
+        //public static double Ortalamahız(double yol, double süre) //parametre alan geri döndüren
+        //{
+        //    double hız = yol / süre;
+        //    return hız;
+
+
+        //}
+
+
+        static void Main(string[] args)
+        {
+        //kavyeden girilen çift sayıları toplayan,tek sayıları çıkartan,0 a basınca programı duran program.
+        //kullanıcın girmiş olduğu sayının basamak değerini veren program
+
+        //long toplam = 0;
+
+        //while (true)
+        //{
+        //    baslangic:
+        //    try
+        //    {
+        //        Console.WriteLine("Lütfen bir sayı giriniz");
+        //        string sayiInput = Console.ReadLine();
+        //        long sayi = long.Parse(sayiInput);
+        //        if (sayi == 0)
+        //        {
+        //            break;
+        //        }
+
+        //        else if (sayi % 2 == 0)
+        //        {
+        //            toplam += sayi;
+        //            Console.WriteLine($"Şuanki toplam={toplam}");
+        //        }
+        //        else if (sayi % 2 != 0)
+        //        {
+        //            toplam -= sayi;
+        //            Console.WriteLine($"Şuanki toplam={toplam}");
+
+        //        }
+
+        //    }
+        //    catch (Exception)
+        //    {
+        //        goto baslangic;
+        //    }
+
+        //    }
+
+        //baslangic:
+        //try
+        //{
+        //    Console.WriteLine("Lütfen bir sayı giriniz.");
+        //    int sayiInput = int.Parse(Console.ReadLine());
+        //    int basamak = 0;
+        //    while (sayiInput > 0)
+        //    {
+        //        basamak++;
+        //        sayiInput = sayiInput / 10;
+
+        //    }
+        //    Console.WriteLine($"Girilen sayı {basamak} basamaklıdır.");
+        //}
+        //catch (Exception) 
+        //{
+
+        //    goto baslangic;
+        //}
+
+        //Bilgisayar tarafından random tutulan sayıyı tahmin eden ve kaçıncı hakkında bildiğimizi yazdıran program.1 ile 30 arasında 
+        //Random rnd = new Random();
+        //int sayac = 1;
+        //int computer = rnd.Next(1, 30);
+
+        //while (true)
+        //{
+
+        //    Console.WriteLine("Sayıyı tahmin ediniz.");
+        //    int kendim=int.Parse(Console.ReadLine());
+        //    if (kendim==computer)
+        //    {
+
+        //        Console.WriteLine($"TEBRİK EDERİM{sayac}");
+        //        return;
+        //    }
+        //    else
+        //    {
+        //        sayac++;
+
+
+        //    }
+           
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+         
+            
+            //parametre alan geri döndüren
+            //Console.WriteLine("kenar1 giriniz");
+            //int a = int.Parse(Console.ReadLine());
+            //Console.WriteLine("kenar2 giriniz");
+            //int b=int.Parse(Console.ReadLine());
+            //Console.WriteLine("kenar3 giriniz");
+            //int c=int.Parse(Console.ReadLine());
+            //Console.WriteLine(Ücgencesidi(a,b,c));
+        }
+        //1 aracın gittiği yol ve giriş süresi klavyeden girildiğinde ortalama hızı hesaplayıp ekrana yazdıran konsol uygulaması
+
+        //Console.WriteLine("Yol uzunluğunu giriniz:   ");                     //parametre alan geri döndüren
+        //    double yol = double.Parse(Console.ReadLine());
+        //Console.WriteLine("Giriş süresini giriniz:   ");
+        //    double süre = double.Parse(Console.ReadLine());
+        //Console.WriteLine(Ortalamahız(yol, süre));
+        //}
+
+       
+                
+                
+
+
+
+
+    }
+}
