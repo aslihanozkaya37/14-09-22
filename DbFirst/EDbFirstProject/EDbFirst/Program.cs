6c3036a0d9613888c5f51f8e665320ef7a59a9b0
aslıhan <aslihan37gungoren@gmail.com>
2022-11-25 14:09:42 +0300

    Dbfirst

---
+++ DbFirst/EDbFirstProject/EDbFirst/Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using EDbFirst.DAL;
+using (var _context=new AppDbContext())
+{
+    var products = _context.Products.ToList();
+    products.ForEach(p =>
+    {
+        Console.WriteLine($"ÜRÜN ID:{p.id}/Ürün Fiyatı:{p.Price} TL");
+    });
+
+}
