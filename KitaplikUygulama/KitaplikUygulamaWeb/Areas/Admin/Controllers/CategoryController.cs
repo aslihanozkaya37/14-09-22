51dbb6c25b945c5a9d8317c40fa87cbca063edd8
aslıhan <aslihan37gungoren@gmail.com>
2022-12-08 16:47:13 +0300

    KİTAPLİİGİM

---
+++ KitaplikUygulama/KitaplikUygulamaWeb/Areas/Admin/Controllers/CategoryController.cs
@@ -0,0 +1,138 @@
+﻿using KitaplikUygulama.DataAccess;
+using KitaplikUygulama.DataAccess.Repository.IRepository;
+using KitaplikUygulama.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace KitaplikUygulamaWeb.Areas.Admin.Controllers
+{
+    
+
+    public class CategoryController : Controller
+    {
+
+        private readonly IUnitOfWork _unitOfWork;
+
+        public CategoryController(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+        public IActionResult Index()
+        {
+            IEnumerable<Category> objCategoryList = _unitOfWork.Category.GetAll();
+            return View(objCategoryList);
+        }
+
+        // CREATE GET
+        [HttpGet]
+        public IActionResult Create()
+        {
+
+            return View();
+        }
+
+
+        //EDİT GET
+        [HttpGet]
+        public IActionResult Edit(int id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+
+            //var categoryFromDb = _db.Categories.Find(id);
+            var categoryFromDbFirst = _unitOfWork.Category.GetFirstOrDefault(u => u.Id == id);
+
+            if (categoryFromDbFirst == null)
+            {
+                return NotFound();
+            }
+
+
+            return View(categoryFromDbFirst);
+        }
+
+        //EDİT POST
+        [HttpPost]
+        public IActionResult Edit(Category obj)
+        {
+            if (obj.Name == obj.DisplayOrder.ToString())
+            {
+                ModelState.AddModelError("CustomError", "Book name cant be equal to Display Order");
+            }
+
+
+            if (ModelState.IsValid)
+            {
+                _unitOfWork.Category.Update(obj);
+                _unitOfWork.Save();
+                TempData["success"] = "Category updated succesfully! :) ";
+                return RedirectToAction("Index", "Category");
+            }
+            return View(obj);
+        }
+
+
+
+        // DELETE GET
+        [HttpGet]
+        public IActionResult Delete(int id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+
+            var categoryFromDb = _unitOfWork.Category.GetFirstOrDefault(u => u.Id == id);
+
+            if (categoryFromDb == null)
+            {
+                return NotFound();
+            }
+
+
+            return View(categoryFromDb);
+        }
+
+        //DELETE POST
+        [HttpPost, ActionName("Delete")]
+        public IActionResult DeletePOST(int id)
+        {
+            var obj = _unitOfWork.Category.GetFirstOrDefault(u => u.Id == id);
+            if (obj == null)
+            {
+                return NotFound();
+            }
+
+            _unitOfWork.Category.Remove(obj);
+            _unitOfWork.Save();
+            TempData["success"] = "Category deleted succesfully! :) ";
+            return RedirectToAction("Index", "Category");
+
+
+        }
+
+
+
+        //CREATE POST
+        [HttpPost]
+        public IActionResult Create(Category obj)
+        {
+            if (obj.Name == obj.DisplayOrder.ToString())
+            {
+                ModelState.AddModelError("CustomError", "Category name cant be equal to Display Order");
+            }
+
+
+            if (ModelState.IsValid)
+            {
+                _unitOfWork.Category.Add(obj);
+                _unitOfWork.Save();
+                TempData["success"] = "Category created succesfully! :) ";
+                return RedirectToAction("Index", "Category");
+            }
+            return View(obj);
+        }
+    }
+}
---
+++ KitaplikUygulama/KitaplikUygulamaWeb/Areas/Admin/Controllers/CoverTypeController.cs
@@ -0,0 +1,129 @@
+﻿using KitaplikUygulama.DataAccess;
+using KitaplikUygulama.DataAccess.Repository.IRepository;
+using KitaplikUygulama.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace KitaplikUygulamaWeb.Areas.Admin.Controllers
+{
+    
+
+    public class CoverTypeController : Controller
+    {
+
+        private readonly IUnitOfWork _unitOfWork;
+
+        public CoverTypeController(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+        public IActionResult Index()
+        {
+            IEnumerable<CoverType> objCoverTypeList = _unitOfWork.CoverType.GetAll();
+            return View(objCoverTypeList);
+        }
+
+        // CREATE GET
+        [HttpGet]
+        public IActionResult Create()
+        {
+
+            return View();
+        }
+
+        //CREATE POST
+        [HttpPost]
+        public IActionResult Create(CoverType obj)
+        {
+            
+            if (ModelState.IsValid)
+            {
+                _unitOfWork.CoverType.Add(obj);
+                _unitOfWork.Save();
+                TempData["success"] = "CoverType created succesfully! :) ";
+                return RedirectToAction("Index", "CoverType");
+            }
+            return View(obj);
+        }
+
+        //EDİT GET
+        [HttpGet]
+        public IActionResult Edit(int id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+
+            
+            var CoverTypeFromDbFirst = _unitOfWork.CoverType.GetFirstOrDefault(u => u.Id == id);
+
+            if (CoverTypeFromDbFirst == null)
+            {
+                return NotFound();
+            }
+
+
+            return View(CoverTypeFromDbFirst);
+        }
+
+        //EDİT POST
+        [HttpPost]
+        public IActionResult Edit(CoverType obj)
+        {
+          
+            if (ModelState.IsValid)
+            {
+                _unitOfWork.CoverType.Update(obj);
+                _unitOfWork.Save();
+                TempData["success"] = "CoverType updated succesfully! :) ";
+                return RedirectToAction("Index", "CoverType");
+            }
+            return View(obj);
+        }
+
+
+
+        // DELETE GET
+        [HttpGet]
+        public IActionResult Delete(int id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+
+            var CoverTypeFromDbFirst = _unitOfWork.CoverType.GetFirstOrDefault(u => u.Id == id);
+
+            if (CoverTypeFromDbFirst == null)
+            {
+                return NotFound();
+            }
+
+
+            return View(CoverTypeFromDbFirst);
+        }
+
+        //DELETE POST
+        [HttpPost, ActionName("Delete")]
+        public IActionResult DeletePOST(int id)
+        {
+            var obj = _unitOfWork.CoverType.GetFirstOrDefault(u => u.Id == id);
+            if (obj == null)
+            {
+                return NotFound();
+            }
+
+            _unitOfWork.CoverType.Remove(obj);
+            _unitOfWork.Save();
+            TempData["success"] = "CoverType deleted succesfully! :) ";
+            return RedirectToAction("Index", "CoverType");
+
+
+        }
+
+
+
+       
+    }
+}
